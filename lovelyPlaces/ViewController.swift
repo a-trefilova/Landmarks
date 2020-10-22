594726d87fad5744151017dbfbe90d50cee6d5d9
Alyona Sabitskaya <trefilovaalyona@gmail.com>
2020-10-22 12:12:12 +0300

    Initial Commit

---
+++ lovelyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lovelyPlaces
+//
+//  Created by Alyona Sabitskaya  on 22.10.2020.
+//  Copyright © 2020 Alyona Sabitskaya . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
